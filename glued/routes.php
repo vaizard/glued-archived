9cfb20d8320278a08aedbae691bad8b4512dc5f2
Pavel Stratil <pavel@vaizard.org>
2016-10-15 22:52:27 +0000

    playing some more with uploads

--- glued/routes.php
+++ glued/routes.php
@@ -30,22 +30,37 @@ $app->group('', function () {
   $this->get('/upload', function ($request, $response, $args) {
       return $this->view->render($response, 'up.twig', $args);
   })->setName('upload');
+
+
   $this->post('/upload', function ($request, $response, $args) {
       $files = $request->getUploadedFiles();
-      if (empty($files['file'])) {
-          throw new Exception('Expected a newfile');
+      if (empty($files['files'])) {
+          throw new Exception('Expected uploaded files, got none.');
       }
-      $newfile = $files['file'];
-      // do something with $newfile
-      if ($newfile->getError() === UPLOAD_ERR_OK) {
-          $uploadFileName = $newfile->getClientFilename();
-          $newfile->moveTo("/var/www/html/glued/private/stor/$uploadFileName");
-          $this->flash->addMessage('info', 'Your file ' . $UploadFileName . ' was successfully uploaded.');
-          return $response->withRedirect($this->router->pathFor('upload'));
-      }
-      $this->flash->addMessage('error', 'Your file upload failed.');
-      return $response->withRedirect($this->router->pathFor('upload'));
 
+      foreach ($files['files'] as $newfile) {
+        if ($newfile->getError() === UPLOAD_ERR_OK) {
+            // rewrite with https://gist.github.com/frostbitten/c1dce70023321158a2fd#file-upload-twig
+            // and https://github.com/brandonsavage/Upload
+            // https://translate.google.cz/translate?hl=cs&sl=zh-CN&tl=en&u=http%3A%2F%2Fwww.php-frameworks.org%2Fforum.php%3Fmod%3Dviewthread%26tid%3D5%26page%3D1%26authorid%3D1&sandbox=1
+            // then look at using vue in slim https://github.com/pklink/machdas
+            $f[]['orig_name'] = $newfile->getClientFilename();
+            $f[]['size'] = $newfile->getClientFilename();
+            $f[]['mime'] = $newfile->getClientFilename();
+            $f[]['result'] = $newfile->getClientFilename();
+            $newfile->moveTo("/var/www/html/glued/private/stor/".$newfile->getClientFilename());
+        } else {
+            $this->flash->addMessage('error', 'Some or all of your files failed to upload.');
+            return $response->withRedirect($this->router->pathFor('upload'));
+        }
+     }
+
+     $success = implode(', ', array_map(function ($entry) {
+        return $entry['orig_name'];
+     }, $f));
+ 
+    $this->flash->addMessage('info', 'All your files (' . $success . ') were successfully uploaded.');
+    return $response->withRedirect($this->router->pathFor('upload'));
   });
 
 
