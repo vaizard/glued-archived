38298f7e8af746afabf48749613672e51bd24b16
Jakub <pohadkar31@seznam.cz>
2017-01-05 13:30:48 +0000

    doplneni realnych souctu k o2

--- glued/Playground/Pohadkar_o2.php
+++ glued/Playground/Pohadkar_o2.php
@@ -78,8 +78,6 @@ class Pohadkar_o2 extends Controller
         else {
             $obsah = file_get_contents($parent_path.'/'.$pozadovane_xml);
             
-            
-            
             // rozparsuju si to sam
             
             preg_match('|<summaryHead.*<\/summaryHead>|Ums', $obsah, $hlavicka);
@@ -96,73 +94,81 @@ class Pohadkar_o2 extends Controller
                 preg_match('|ownerCustCode="([^"]*)"|', $subscriber, $customcode);
                 preg_match('|summaryPrice="([^"]*)"|', $subscriber, $sumprice);
                 
-                $vystup .= '
-                <div class="panel panel-primary">
-                  <div class="panel-heading">Phone <strong>'.$phonenumber[1].'</strong></div>
-                  <div class="panel-body">
-                    Owner '.$ownernumber[1].'<br>
-                    code: '.$customcode[1].'<br>
-                    summary: '.$sumprice[1].'
-                  </div>
-                </div>';
-                
-                
                 
                 // regular charges
-                $vystup .= '<table class="table table-bordered">';
-                $vystup .= '<tr class="info"><th>Regular charges</th><th>from</th><th>to</th><th>price without tax</th><th>tax</th></tr>';
+                $rc_vystup = '';
+                $rc_real_summary = 0;
+                $rc_vystup .= '<table class="table table-bordered">';
+                $rc_vystup .= '<tr class="info"><th>Regular charges</th><th>from</th><th>to</th><th>price without tax</th><th>tax</th></tr>';
                 preg_match('|<regularCharges.*<\/regularCharges>|Ums', $subscriber, $regularblok);
                 preg_match('|rcTotalPrice="([^"]*)"|', $regularblok[0], $rcprice);
                 preg_match_all('|<rcItem [^>]*>|', $regularblok[0], $rcitemy);
                 foreach ($rcitemy[0] as $rcitem) {
                     $rcdata = simplexml_load_string($rcitem);
                     $rcattr = $rcdata->attributes();
-                    $vystup .= '<tr><td>'.$rcattr['feeName'].'</td><td>'.$rcattr['validFrom'].'</td><td>'.$rcattr['validTo'].'</td><td>'.$rcattr['priceWithoutTax'].'</td><td>'.$rcattr['tax'].'</td></tr>';
+                    $rc_vystup .= '<tr><td>'.$rcattr['feeName'].'</td><td>'.$rcattr['validFrom'].'</td><td>'.$rcattr['validTo'].'</td><td>'.$rcattr['priceWithoutTax'].'</td><td>'.$rcattr['tax'].'</td></tr>';
+                    $rc_real_summary += (float) $rcattr['priceWithoutTax'];
                 }
-                $vystup .= '<tr class="success"><td colspan="5"> total regular charges price: <strong>'.$rcprice[1].'</strong></td></tr>';
-                $vystup .= '</table>';
+                $rc_vystup .= '<tr class="active"><td colspan="5"> total regular charges price: <strong>'.$rcprice[1].'</strong> (real summary: '.$rc_real_summary.')</td></tr>';
+                $rc_vystup .= '</table>';
+                
                 
                 // usage charges
+                $uc_vystup = '';
+                $uc_real_summary = 0;
                 $testuj_usage = preg_match('|<usageCharges.*<\/usageCharges>|Ums', $subscriber, $usageblok);
                 if ($testuj_usage) {
-                    $vystup .= '<table class="table table-bordered">';
-                    $vystup .= '<tr class="info"><th>Usage charges</th><td>quantity + uom</td><td>displayedUom</td><td>period</td><td>tax</td><td>priceWithoutTax</td><td>freeUnitsAmount</td></tr>';
+                    $uc_vystup .= '<table class="table table-bordered">';
+                    $uc_vystup .= '<tr class="info"><th>Usage charges</th><td>quantity + uom</td><td>displayedUom</td><td>period</td><td>tax</td><td>priceWithoutTax</td><td>freeUnitsAmount</td></tr>';
                     preg_match('|<usageCharges.*<\/usageCharges>|Ums', $subscriber, $usageblok);
                     preg_match('|ucTotalPrice="([^"]*)"|', $usageblok[0], $ucprice);
                     // ted jednotlive charge
                     preg_match_all('|<usageCharge.*<\/usageCharge>|Ums', $usageblok[0], $subusagebloky);
                     foreach ($subusagebloky[0] as $subusage) {
+                        $real_items_summary = 0;
                         preg_match('|usagePackName="([^"]*)"|', $subusage, $subname);
                         preg_match('|subtotalPrice="([^"]*)"|', $subusage, $subprice);
-                        $vystup .= '<tr><td colspan="7"><strong>'.$subname[1].', subtotal price: '.$subprice[1].'</strong></td></tr>';
                         preg_match_all('|<ucItem [^>]*>|', $subusage, $ucitemy);
+                        $sub_real_sumary = 0;
+                        $sub_uc_vystup = '';
                         foreach ($ucitemy[0] as $ucitem) {
+                            /*
+                            <ucItem quantity="3840" displayedUom="min" uom="Sec" periodDescr="špička" period="02" tax="21.0" rowID="1" quantityOfConnect="21" priceWithoutTax="49.5" parentRowID="901" name="Do O2" freeUnitsPrice="0.0" freeUnitsAmount="870.0" freeCredits="0"/>
+                            */
                             $ucdata = simplexml_load_string($ucitem);
                             $ucattr = $ucdata->attributes();
-                            $vystup .= '<tr><td>'.$ucattr['name'].'</td><td>'.$ucattr['quantity'].' '.$ucattr['uom'].'</td><td>'.$ucattr['displayedUom'].'</td><td>'.$ucattr['periodDescr'].'</td><td>'.$ucattr['tax'].'</td><td>'.$ucattr['priceWithoutTax'].'</td><td>'.$ucattr['freeUnitsAmount'].'</td></tr>';
+                            $sub_uc_vystup .= '<tr><td>'.$ucattr['name'].'</td><td>'.$ucattr['quantity'].' '.$ucattr['uom'].'</td><td>'.$ucattr['displayedUom'].'</td><td>'.$ucattr['periodDescr'].'</td><td>'.$ucattr['tax'].'</td><td>'.$ucattr['priceWithoutTax'].'</td><td>'.$ucattr['freeUnitsAmount'].'</td></tr>';
+                            $sub_real_sumary += (float) $ucattr['priceWithoutTax'];
                         }
+                        $uc_vystup .= '<tr><td colspan="7"><strong>'.$subname[1].', subtotal price: '.$subprice[1].'</strong> (real summary: '.$sub_real_sumary.')</td></tr>';
+                        $uc_vystup .= $sub_uc_vystup;
+                        $uc_real_summary += $sub_real_sumary;
                     }
-                    $vystup .= '<tr class="success"><td colspan="7">total usage charges price: <strong>'.$ucprice[1].'</strong></td></tr>';
-                    $vystup .= '</table>';
+                    $uc_vystup .= '<tr class="active"><td colspan="7">total usage charges price: <strong>'.$ucprice[1].'</strong> (real summary: '.$uc_real_summary.')</td></tr>';
+                    $uc_vystup .= '</table>';
                 }
                 
-                /*
-                quantity + uom
-          - displayedUom=min
-          - periodDescr=
-          - name
-          - tax="21.0"
-          - priceWithoutTax="21.0"
-          - freeUnitsAmount="870.0"
-          
-          <ucItem quantity="3840" displayedUom="min" uom="Sec" periodDescr="špička" period="02" tax="21.0" rowID="1" quantityOfConnect="21" priceWithoutTax="49.5" parentRowID="901" name="Do O2" freeUnitsPrice="0.0" freeUnitsAmount="870.0" freeCredits="0"/>
-                */
                 
                 // free units
                 
+                // vystup
+                
+                $vystup .= '
+                <div class="panel panel-primary" style="margin-top: 60px;">
+                  <div class="panel-heading">Phone <strong>'.$phonenumber[1].'</strong></div>
+                  <div class="panel-body">
+                    Owner '.$ownernumber[1].'<br>
+                    code: '.$customcode[1].'<br>
+                    summary: '.$sumprice[1].' (real summary: '.($rc_real_summary + $uc_real_summary).')
+                  </div>
+                </div>';
+                
+                $vystup .= $rc_vystup;
+                $vystup .= $uc_vystup;
                 
             }
             
+            /*
             $p = xml_parser_create();
             xml_parse_into_struct($p, $obsah, $vals);
             xml_parser_free($p);
@@ -179,6 +185,8 @@ class Pohadkar_o2 extends Controller
                     $vystup .= '</div>';
                 }
             }
+            */
+            
         }
         
         
