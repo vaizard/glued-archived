f2cea5113362e841051d4ccb0c28c52d8fb80497
Jakub <kubi2x@centrum.cz>
2019-03-13 12:16:56 +0100

    oprava consumables

--- glued/Controllers/Consumables/ConsumablesController.php
+++ glued/Controllers/Consumables/ConsumablesController.php
@@ -69,7 +69,7 @@ class ConsumablesController extends Controller
         $json_uischema_output = file_get_contents(__DIR__.'/V1/jsonuischemas/form_ui.json');
         
         // schema celeho formulare
-        $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/new_consumables_form.json');
+        $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/consumables.json');
         
         // zakladni data, momentalne nic, ale musi tam byt aspon prazdny json objekt, tedy ty slozene zavorky
         $json_formdata_output = '{"data":{"ts_created":"'.time().'","ts_updated":"'.time().'"}}';
@@ -119,7 +119,7 @@ class ConsumablesController extends Controller
         $json_uischema_output = file_get_contents(__DIR__.'/V1/jsonuischemas/form_ui_quick.json');
         
         // schema celeho formulare
-        $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/new_consumables_form.json');
+        $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/consumables.json');
         
         // zakladni data, momentalne nic, ale musi tam byt aspon prazdny json objekt, tedy ty slozene zavorky
         $json_formdata_output = '{"data":{"ts_created":"'.time().'","ts_updated":"'.time().'"}}';
@@ -203,7 +203,7 @@ class ConsumablesController extends Controller
             $json_uischema_output = file_get_contents(__DIR__.'/V1/jsonuischemas/form_ui.json');
             
             // schema celeho editacniho formulare. je to prakticky shodne schema jako formular pro novy bill, krome title
-            $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/edit_consumables_form.json');
+            $json_schema_output = file_get_contents(__DIR__.'/V1/jsonschemas/consumables.json');
             
             // zakladni data pro editaci
             $json_formdata_output = $data['c_data'];
