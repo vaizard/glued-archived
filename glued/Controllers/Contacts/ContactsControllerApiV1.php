043c85945fd0d0263e731500a7420feb831edb4f
Jakub <kubi2x@centrum.cz>
2019-05-18 12:42:58 +0200

    contacts two collumn

--- glued/Controllers/Contacts/ContactsControllerApiV1.php
+++ glued/Controllers/Contacts/ContactsControllerApiV1.php
@@ -13,10 +13,21 @@ class ContactsControllerApiV1 extends Controller
         $senddata = $request->getParam('billdata');
         $user_id = $_SESSION['user_id'];
         
+        // upravime send data, co je v left_column a v right_column dame o uroven nahoru
+        // musime to upravovat jako objekt
+        $send_data_object = json_decode($senddata);
+        $left_data = $send_data_object->data->left_column;
+        $right_data = $send_data_object->data->right_column;
+        unset($send_data_object->data->left_column);
+        unset($send_data_object->data->right_column);
+        foreach($left_data as $k => $v) { $send_data_object->data->$k = $v; }
+        foreach($right_data as $k => $v) { $send_data_object->data->$k = $v; }
+        $senddata_upravena = json_encode($send_data_object);
+        
         // vlozime to jak to prislo z formu
         // 500 = 111 110 100
         $data = Array ("c_owner" => $user_id, "c_group" => 2, "c_unixperms" => 500,
-                       "c_data" => $senddata
+                       "c_data" => $senddata_upravena
         );
         $insert = $this->container->db->insert('t_contacts', $data);
         if ($insert) {
@@ -24,7 +35,7 @@ class ContactsControllerApiV1 extends Controller
             $this->container->db->rawQuery("UPDATE t_contacts SET c_data = JSON_SET(c_data, '$.data._id', ?, '$.data.creator', ?, '$.data.dt_created', ?) WHERE c_uid = ? ", Array (strval($insert), strval($user_id), date("Y-m-d H:i:s"), $insert));
             
             if ($this->container->db->getLastErrno() === 0) {
-                $response->getBody()->write('ok');
+                $response->getBody()->write($senddata_upravena);
             }
             else {
                 $response->getBody()->write('update failed: ' . $this->container->db->getLastError());
@@ -45,11 +56,22 @@ class ContactsControllerApiV1 extends Controller
         
         $senddata = $request->getParam('billdata');
         
+        // upravime send data, co je v left_column a v right_column dame o uroven nahoru
+        // musime to upravovat jako objekt
+        $send_data_object = json_decode($senddata);
+        $left_data = $send_data_object->data->left_column;
+        $right_data = $send_data_object->data->right_column;
+        unset($send_data_object->data->left_column);
+        unset($send_data_object->data->right_column);
+        foreach($left_data as $k => $v) { $send_data_object->data->$k = $v; }
+        foreach($right_data as $k => $v) { $send_data_object->data->$k = $v; }
+        $senddata_upravena = json_encode($send_data_object);
+        
         $this->container->db->where('c_uid', $args['id']);
-        $update = $this->container->db->update('t_contacts', Array ( 'c_data' => $senddata ));
+        $update = $this->container->db->update('t_contacts', Array ( 'c_data' => $senddata_upravena ));
         
         // vratime prosty text
-       $response->getBody()->write('ok');
+       $response->getBody()->write($senddata_upravena);
        return $response;
         
     }
